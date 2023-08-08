3a11d5325caa218641d4e2b6d2918801c066af5e
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-08-08 15:41:37 -0700

    hi

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-
-
-def num(x):
-    y = abs(x) % 10
-    if x < 0:
-        y *= -1
-    if (y >= 6):
-        print(f"Last digit of {x} is {y} and is greater than 5")
-    elif(y == 0):
-        print(f"Last digit of {x} is {y} and is 0")
-    else:
-        print(f"Last digit of {x} is {y} and is less than 6 and not 0")
-num(number)
+def add(a, b):
+    return a + b
