cafed836b921c39ddd5eff4a102a46cd33a0247e
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-08-08 14:55:29 -0700

    hi

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+
+def num(x):
+    if (x < 0):
+        print(x, "is negative")
+    elif(x == 0):
+        print(x, "is zero")
+    else:
+        print(x, "is positive")
+num(number)
