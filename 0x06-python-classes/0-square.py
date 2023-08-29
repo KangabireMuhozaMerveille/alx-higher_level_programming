7d652433d9d563d1e160dbc2281b0fa5180cb6dc
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-08-29 14:35:49 -0700

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this is a code that defines an empty square
+"""
+
+
+class Square:
+    """
+    this class represents square
+    """
+    def square(self):
+        pass
